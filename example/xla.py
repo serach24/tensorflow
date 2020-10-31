d7858124baca526808d83390723e34b727a58ed0
Anand J <anandj@cs.toronto.edu>
2020-10-31 18:27:47 -0400

    xla.py code moved to use tf v2.4 API

--- example/xla.py
+++ example/xla.py
@@ -6,16 +6,11 @@ os.environ['TF_DUMP_GRAPH_PREFIX'] = '/tmp/tf_graphs/'
 import numpy as np
 import tensorflow as tf
 
-from tensorflow.contrib.compiler import xla
-
 inp = np.random.rand(10, 10)
 
+@tf.function(experimental_compile=True)
 def model_fn(x):
     y = tf.sigmoid(tf.matmul(x, x))
     return y
 
-x = tf.placeholder(tf.float32, shape=(10, 10))
-[y] = xla.compile(model_fn, inputs=[x])
-
-with tf.Session() as sess:
-    print(sess.run(y, feed_dict={x: inp}))
+print(model_fn(inp))
