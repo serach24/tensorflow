09113436299f83a6a0b673faeee659b9fb9c3d28
Anand J <anandj@cs.toronto.edu>
2020-11-04 23:29:11 -0500

    Add XLA_FLAGS

--- example/xla_v1.py
+++ example/xla_v1.py
@@ -2,6 +2,7 @@ import os
 os.environ['TF_CPP_MIN_VLOG_LEVEL'] = '3'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
 os.environ['TF_DUMP_GRAPH_PREFIX'] = '/tmp/tf_graphs/'
+os.environ['XLA_FLAGS'] = '--xla_dump_hlo_as_text --xla_dump_to=/tmp/xla/custom'
 
 import numpy as np
 import tensorflow as tf
